c56f443a173edc9d67a6e8c57ca8d4e48f852d1a
kolin98 <cpt.beatle@gmail.com>
2026-01-15 01:08:07 +0100

    add shadcn

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,13 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "core": path.resolve(__dirname, "../../packages/core/src")
-    }
-  }
-})
+    plugins: [react(), tailwindcss()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+            core: path.resolve(__dirname, "../../packages/core/src"),
+        },
+    },
+});
