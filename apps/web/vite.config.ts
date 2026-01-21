95df7eb38b777f48580918ff6f1caf552fc1001a
kolin98 <cpt.beatle@gmail.com>
2026-01-21 02:47:42 +0100

    cicd: wrangler config

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-import path from "path";
+import { cloudflare } from '@cloudflare/vite-plugin';
 import tailwindcss from "@tailwindcss/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [TanStackRouterVite(), react(), tailwindcss()],
+    plugins: [tanstackRouter(), react(), tailwindcss(), cloudflare()],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
